bc9a113a948a0e687fa8ccaee4ade90f9eab0ddb
yazanramadan9 <yazanramadan9999@gmail.com>
2023-03-22 23:40:23 +0300

    products front end functionality tiny fix

--- pages/app/products.jsx
+++ pages/app/products.jsx
@@ -17,40 +17,6 @@ import useConfirmDialog from "../../hooks/use-confirm-dialog";
 import ConfirmModal from "../../components/common/confirm-dialog";
 import objectPath from "object-path";
 
-// export interface CarouselProps {
-//    responsive: responsiveType;
-//    deviceType?: string;
-//    ssr?: boolean;
-//    slidesToSlide?: number;
-//    draggable?: boolean;
-//    arrows?: boolean;
-//    swipeable?: boolean;
-//    removeArrowOnDeviceType?: string | Array<string>;
-//    children: any;
-//    customLeftArrow?: React.ReactElement<any> | null;
-//    customRightArrow?: React.ReactElement<any> | null;
-//    customDot?: React.ReactElement<any> | null;
-//    customButtonGroup?: React.ReactElement<any> | null;
-//    infinite?: boolean;
-//    minimumTouchDrag?: number;
-//    afterChange?: (previousSlide: number, state: stateCallBack) => void;
-//    beforeChange?: (nextSlide: number, state: stateCallBack) => void;
-//    sliderClass?: string;
-//    itemClass?: string;
-//    containerClass?: string;
-//    dotListClass?: string;
-//    keyBoardControl?: boolean;
-//    centerMode?: boolean;
-//    autoPlay?: boolean;
-//    autoPlaySpeed?: number;
-//    showDots?: boolean;
-//    partialVisbile?: boolean;
-//    customTransition?: string;
-//    transitionDuration?: number;
-//    focusOnSelect?: boolean;
-//    additionalTransfrom?: number;
-// }
-
 class ProductFuture {
    id;
    product_id;
