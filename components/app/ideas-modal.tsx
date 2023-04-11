1455ec3ff674f4808a545865d659a74d427ebb42
yazanramadan9 <yazanramadan9999@gmail.com>
2023-04-11 08:29:57 +0300

    ideas is refactored

--- components/app/ideas-modal.tsx
+++ components/app/ideas-modal.tsx
@@ -1,12 +1,11 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useState } from "react";
-import * as Yup from "yup";
 import Spinner from "../common/spinner";
 
-import ideas_dummy from "../../samples/ideas.json";
-import { Idea } from "../../models/idea.model";
+import products_dummy from "../../samples/products.json";
 import { NextPage } from "next";
 import Modal from "../common/modal";
+import IdeasProduct from "../ideas/ideas-product";
+import { ProductModel } from "../../models/products/product.model";
 
 type Props = {
    isOpen: boolean;
@@ -14,7 +13,7 @@ type Props = {
 };
 
 const IdeasModal: NextPage<Props> = ({ isOpen, toggle }) => {
-   const [ideas, setIdeas] = useState(ideas_dummy);
+   const [products, setProducts] = useState(products_dummy);
 
    if (!isOpen) return <></>;
 
@@ -22,92 +21,35 @@ const IdeasModal: NextPage<Props> = ({ isOpen, toggle }) => {
       <>
          <Modal
             config={{
-               className: "p-5 lg:p-10 modal-content",
+               className: "p-5 lg:p-10 modal-content h-[90%] max-h-[650px]",
                isShown: isOpen,
                closeCallback: toggle,
             }}>
-            <div className='flex justify-between p-3 h-12 mb-10'>
-               <div className='modal-title mb-10'>
-                  <h2 className='mb-4 text-[2.8rem] text-gray-800'>Ideas</h2>
-                  <h3 className='mb-6 f6 text-gray-gunmetal'>Add your ideas</h3>
+            <div className='flex justify-between p-3 mb-2'>
+               <div className='flex flex-col gap-5'>
+                  <h2 className='text-4xl text-gray-700'>Products Ideas</h2>
+                  <h3 className='text-gray-400 text-2xl'>
+                     Add ideas to your products
+                  </h3>
                </div>
                <button
                   type='button'
-                  className='modal-close dr-close'
+                  className='self-start modal-close dr-close'
                   onClick={toggle}
                   aria-label='Close'></button>
             </div>
-            <div
-               className='mt-10 relative flex-auto p-3 overflow-auto'
-               id='ideas-app'>
-               <div className='idea-list'>
-                  {!!ideas.length && (
-                     <ul className='flex flex-col gap-3 mb-5'>
-                        {ideas.map((idea: Idea, index: number) => (
-                           <li key={index} className='text-gray-800'>
-                              <span> {idea.text} </span>
-                              <button
-                                 onClick={() => {
-                                    console.log("delete idea ", idea.id);
-                                 }}
-                                 type='button'>
-                                 remove
-                              </button>
-                           </li>
-                        ))}
-                     </ul>
-                  )}
+            {!products.length && (
+               <p className='text-rose-300'>You Have No Products yet !</p>
+            )}
+            {!!products.length && (
+               <div className='h-[90%] overflow-auto'>
+                  {products.map((product: ProductModel, index: number) => (
+                     <div key={index} className='border-b'>
+                        <IdeasProduct product={product} />
+                     </div>
+                  ))}
                </div>
-               <Formik
-                  initialValues={{
-                     idea: "",
-                  }}
-                  validationSchema={Yup.object({
-                     idea: Yup.object({
-                        id: Yup.string(),
-                        name: Yup.string().required("required"),
-                     }),
-                  })}
-                  onSubmit={(values) => {
-                     console.log(values);
-                  }}
-                  validateOnMount>
-                  {({ values, isSubmitting, isValid }) => (
-                     <Form>
-                        <div>
-                           <Field
-                              type='text'
-                              className='w-full p-3 bg-gray-100 outline-none caret-dark-blue border-none'
-                              placeholder='New idea'
-                              name='idea.name'
-                           />
-                           <ErrorMessage name={`idea.name`}>
-                              {(msg) => (
-                                 <div className='text-lg text-rose-500'>
-                                    {msg}
-                                 </div>
-                              )}
-                           </ErrorMessage>
-                        </div>
-                        <div className='flex justify-end pt-10'>
-                           <button
-                              type='submit'
-                              className={
-                                 isSubmitting || !isValid
-                                    ? "btn-rev btn-disabled"
-                                    : "btn-rev"
-                              }
-                              disabled={isSubmitting || !isValid}>
-                              Save
-                           </button>
-                        </div>
-                        {isSubmitting && (
-                           <Spinner className='' message='Saving Ideas' />
-                        )}
-                     </Form>
-                  )}
-               </Formik>
-            </div>
+            )}
          </Modal>
       </>
    );
--- models/idea.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Idea {
-   id: string;
-   text: string;
-}
