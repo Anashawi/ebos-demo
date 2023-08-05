4ded58b307845b6eac4024244c93d2a7761867df
Mustafa Khairy <alkhairy.m@gmail.com>
2023-08-05 16:44:02 +0300

    rollback fix download pdf trail 1

--- next.config.js
+++ next.config.js
@@ -11,18 +11,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
