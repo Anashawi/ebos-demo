854467c24b728ca7f06d3dc98092fe650e2ab35a
Mustafa Khairy <alkhairy.m@gmail.com>
2023-09-07 16:26:47 +0300

    apply tanstack dev tool on app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/report.css";
 import "../styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 // Tell Font Awesome to skip adding the CSS automatically
 // since it's already imported above
@@ -19,6 +20,7 @@ const App: NextPage<Props> = ({ Component, pageProps }) => {
 		<SessionProvider session={pageProps.session}>
 			<QueryClientProvider client={queryClient}>
 				<Component {...pageProps} />
+				<ReactQueryDevtools />
 			</QueryClientProvider>
 		</SessionProvider>
 	);
