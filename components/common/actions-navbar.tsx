6683bc678b254f466efea4fc01292a2efdf98b5b
IbrahimWehbi <ibrahimmwehbi@gmail.com>
2023-12-17 09:25:24 +0200

    place disable/enable chatGPT button in actions side panel, fix button center issue

--- components/common/actions-navbar.tsx
+++ components/common/actions-navbar.tsx
@@ -1,15 +1,17 @@
+import { useContext } from "react";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { stepNamesEnum, tooltipDirectionsEnum } from "../../models/enums";
+import { stepNamesEnum, tooltipDirectionsEnum, ChatIs } from "../../models/enums";
+import { chatBoxStateData } from "../../context";
 
 import ConsultantReview from "./consultant-review";
 import Tooltip from "./tooltip";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket, faComment, faCommentSlash } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
     selectedStepTitle: stepNamesEnum;
@@ -18,41 +20,25 @@ interface Props {
 const ActionsNavbar = ({ selectedStepTitle: displayedPageTitle }: Props) => {
     const { data: session }: any = useSession();
 
+    const { chatBoxState, setChatBoxState } = useContext(chatBoxStateData);
+
     return (
         <nav className="main-navigation">
+            {/* the parent of Tooltip must have the following css classes: "group relative" to work properly */}
             <main className="nav-section-ul">
                 <li>
-                    <Image
-                        width="55"
-                        height="55"
-                        src="/cp.svg"
-                        alt="CaseInPoint"
-                    />
+                    <Image width="55" height="55" src="/cp.svg" alt="CaseInPoint" />
                 </li>
                 <li className="group relative hover:animate-shake">
                     <Link href="/">
-                        <Image
-                            width="42"
-                            height="42"
-                            src="/dashboard.svg"
-                            alt="Dashboard"
-                        />
+                        <Image width="42" height="42" src="/dashboard.svg" alt="Dashboard" />
                     </Link>
-                    {/* the parent of Tooltip must have the following css classes: "group relative" to work properly */}
-                    <Tooltip dir={tooltipDirectionsEnum.bottom}>
-                        Dashboard
-                    </Tooltip>
+                    <Tooltip dir={tooltipDirectionsEnum.bottom}>Dashboard</Tooltip>
                 </li>
                 <li className="group relative hover:animate-shake">
                     <ConsultantReview pageTitle={displayedPageTitle}>
-                        <Image
-                            width="45"
-                            height="45"
-                            src="/consultations.svg"
-                            alt="Consultations"
-                        />
+                        <Image width="45" height="45" src="/consultations.svg" alt="Consultations" />
                     </ConsultantReview>
-                    {/* the parent of Tooltip must have the following css classes: "group relative" to work properly */}
                     <Tooltip dir={tooltipDirectionsEnum.bottom}>
                         <p className="w-max">
                             <strong>Request </strong> for consultant review
@@ -62,23 +48,39 @@ const ActionsNavbar = ({ selectedStepTitle: displayedPageTitle }: Props) => {
             </main>
             <ul className="nav-section-ul">
                 <li>
-                    <p className="text-center font-hero-semibold text-[1.3rem] text-[#4e79b2]">
-                        {session?.user?.fullName}
-                    </p>
+                    <p className="text-center font-hero-semibold text-4 text-[#4e79b2]">{session?.user?.fullName}</p>
                 </li>
                 <li
-                    className="group relative w-[4rem] h-[4rem] p-4 bg-icons-gray rounded-full hover:p-4 hover:btn-danger hover:cursor-pointer transition duration-200"
+                    className={`group relative w-16 h-16 p-4 flex leading-7 bg-icons-gray rounded-full cursor-pointer ${
+                        chatBoxState === ChatIs.Disabled ? `hover:bg-blue-900` : `hover:bg-red-600`
+                    } transition duration-200`}
+                    onClick={() => {
+                        chatBoxState === ChatIs.Disabled
+                            ? setChatBoxState(ChatIs.Minimized)
+                            : setChatBoxState(ChatIs.Disabled);
+                    }}
+                >
+                    {chatBoxState === ChatIs.Disabled ? (
+                        <>
+                            <FontAwesomeIcon icon={faComment} className="w-8 h-8 text-white" />
+                            <Tooltip dir={tooltipDirectionsEnum.bottom}>Enable Chat</Tooltip>
+                        </>
+                    ) : (
+                        <>
+                            <FontAwesomeIcon icon={faCommentSlash} className="w-8 h-8 text-white" />
+                            <Tooltip dir={tooltipDirectionsEnum.bottom}>Disable Chat</Tooltip>
+                        </>
+                    )}
+                </li>
+                <li
+                    className="group relative w-16 h-16 p-4 bg-icons-gray rounded-full cursor-pointer hover:bg-red-600 transition duration-200"
                     onClick={() => {
                         signOut({
                             callbackUrl: "/",
                         });
                     }}
                 >
-                    <FontAwesomeIcon
-                        icon={faArrowRightFromBracket}
-                        className="text-white"
-                    />
-                    {/* the parent of Tooltip must have the following css classes: "group relative" to work properly */}
+                    <FontAwesomeIcon icon={faArrowRightFromBracket} className="text-white" />
                     <Tooltip dir={tooltipDirectionsEnum.bottom}>Logout</Tooltip>
                 </li>
             </ul>
